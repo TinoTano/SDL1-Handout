3142545177a52890de2c357b04f9aacb9afb6809
Adrià Martín <elsuperchowchow@gmail.com>
2016-03-14 20:12:34 +0100

    SDL1 Handout

---
+++ SDL1_Handout/Globals.h
@@ -0,0 +1,36 @@
+#ifndef __GLOBALS_H__
+#define __GLOBALS_H__
+
+#include <windows.h>
+#include "SDL\include\SDL.h"
+#include "SDL_image\include\SDL_image.h"
+#include "SDL_mixer\include\SDL_mixer.h"
+
+#pragma comment( lib, "SDL/libx86/SDL2.lib" )
+#pragma comment( lib, "SDL/libx86/SDL2main.lib" )
+#pragma comment( lib, "SDL_image/libx86/SDL2_image.lib" )
+#pragma comment(lib, "SDL_mixer/libx86/SDL2_mixer.lib")
+
+#define LOG(txt) OutputDebugString(txt)
+
+#define EXIT_FAILURE 1
+#define EXIT_SUCCESS 0
+
+enum update_status
+{
+	UPDATE_CONTINUE = 1,
+	UPDATE_STOP,
+	UPDATE_ERROR
+};
+
+struct globals
+{
+	SDL_Window* window;
+	SDL_Surface* w_surface;
+	SDL_Renderer* renderer;
+	SDL_Texture* background;
+	int background_width;
+	Mix_Music* background_music;
+} g;
+
+#endif // __GLOBALS_H__
