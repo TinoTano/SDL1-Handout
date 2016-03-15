e6a248936a3e8f94b7d42ad52b7c1adfce08dd01
Adrià Martín <elsuperchowchow@gmail.com>
2016-03-15 12:05:22 +0100

    Music fully working

--- SDL1_Handout/SDL1_Handout/MusicSDLModule.h
+++ SDL1_Handout/SDL1_Handout/MusicSDLModule.h
@@ -11,24 +11,29 @@ class ModuleMusic : public Module
 		LOG("\nMusicSDLModule Init!");
 		SDL_Init(SDL_INIT_EVERYTHING);
 		Mix_Init(MIX_INIT_OGG);
+		Mix_OpenAudio(MIX_DEFAULT_FREQUENCY, MIX_DEFAULT_FORMAT, 2, 1024);
 		g.background_music = Mix_LoadMUS("music.ogg");
 		if (g.background_music == NULL)
 		{
-			std::cout << "Music not loaded";
+			std::cout << "Unable to load Ogg file: \n" << Mix_GetError() << "\n";
 		}
 		else{
-			std::cout << "Music loaded";
+			std::cout << "Music loaded! \n";
 		}
-		Mix_PlayMusic(g.background_music, -1);
+		if (Mix_PlayMusic(g.background_music, -1) == -1) { 
+			std::cout << "Unable to play Ogg file: " << Mix_GetError() << "\n";
+			return 1; 
+		}
 		return true;
 	}
 
 	bool CleanUp()
 	{
-		LOG("\nMusicSDLModule CleanUp!");
+		LOG("\nMusicSDLModule CleanUp!\n");
 		Mix_FreeMusic(g.background_music);
 		g.background_music = NULL;
 		Mix_Quit();
+		SDL_Quit();
 		return true;
 	}
 };
